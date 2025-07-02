10ed4424e06966e3abb065bc2e6b52cc876f979f
dangtrantrung <trung280584@gmail.com>
2025-07-02 13:17:27 +0700

    Api route is working - Test json

---
+++ api/controllers/user.controller.js
@@ -0,0 +1,6 @@
+export const test = (req, res) => {
+    //res.send('test hello world')
+    res.json({
+        message: "Api router is working - Test json",
+    })
+}
--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import mongoose from 'mongoose'
 import dotenv from 'dotenv'
+import userRouter from './routes/user.router.js'
 dotenv.config()
 const app = express()
 const port = 5000
@@ -10,3 +11,12 @@ app.listen(port, () => {
 mongoose.connect(process.env.MONGO).then(() => {
   console.log('Connected to Mongo DB')
 }).catch((err) => { console.log(err) })
+
+//api routes
+// app.get('/test', (req, res) => {
+//   //res.send('test hello world')
+//   res.json({
+//     message: "Test json",
+//   })
+// })
+app.use('/api/user', userRouter)
