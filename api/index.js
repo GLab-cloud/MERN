ffb1cd81cb7f9fcd01892d479d21aad0854b335e
dangtrantrung <trung280584@gmail.com>
2025-07-02 20:39:57 +0700

    middleware for comprehensive error process

--- api/index.js
+++ api/index.js
@@ -23,3 +23,14 @@ mongoose.connect(process.env.MONGO).then(() => {
 // })
 app.use('/api/user', userRouter)
 app.use('/api/auth', authRouter)
+
+//middlwware for comprehensive error process
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || 'Internal Server Error!!';
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+})
