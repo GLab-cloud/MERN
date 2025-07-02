c5f964e08204d3930fcb6c5eb869d7d25f7d0cae
dangtrantrung <trung280584@gmail.com>
2025-07-02 12:54:39 +0700

    Create User Model

--- api/index.js
+++ api/index.js
@@ -2,9 +2,11 @@ import express from 'express'
 import mongoose from 'mongoose'
 import dotenv from 'dotenv'
 dotenv.config()
-mongoose.connect(process.env.MONGO)
 const app = express()
 const port = 5000
 app.listen(port, () => {
   console.log('Website served on http://localhost:' + port)
 })
+mongoose.connect(process.env.MONGO).then(() => {
+  console.log('Connected to Mongo DB')
+}).catch((err) => { console.log(err) })
