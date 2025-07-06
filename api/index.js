2972efe6df264b796708f632b0b953b5d500867e
GLab <54309362+GLab-cloud@users.noreply.github.com>
2025-07-06 06:53:17 +0000

    codesandbox

--- api/index.js
+++ api/index.js
@@ -1,25 +1,32 @@
-import express from 'express'
-import mongoose from 'mongoose'
-import dotenv from 'dotenv'
-import userRouter from './routes/user.router.js'
-import authRouter from './routes/auth.route.js'
-import cors from 'cors'
-dotenv.config()
-const app = express()
-app.use(express.json())
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import userRouter from "./routes/user.router.js";
+import authRouter from "./routes/auth.route.js";
+import cors from "cors";
+dotenv.config();
+const app = express();
+app.use(express.json());
 app.use(
   cors({
     credentials: true,
-    origin: ['http://localhost:3000'],
-  }),
-)
-const port = 5000
+    origin: ["http://localhost:3000"],
+  })
+);
+const port = 5000;
+const MONGO_DB =
+  "mongodb+srv://tintin:DT4tgJjzkifrCrND@cluster0.5usukl0.mongodb.net/RealEstate?retryWrites=true&w=majority";
 app.listen(port, () => {
-  console.log('Website served on http://localhost:' + port)
-})
-mongoose.connect(process.env.MONGO_DB).then(() => {
-  console.log('Connected to Mongo DB')
-}).catch((err) => { console.log(err) })
+  console.log("Website served on http://localhost:" + port);
+});
+mongoose
+  .connect(MONGO_DB)
+  .then(() => {
+    console.log("Connected to Mongo DB");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 //api routes
 // app.get('/test', (req, res) => {
@@ -28,16 +35,16 @@ mongoose.connect(process.env.MONGO_DB).then(() => {
 //     message: "Test json",
 //   })
 // })
-app.use('/api/user', userRouter)
-app.use('/api/auth', authRouter)
+app.use("/api/user", userRouter);
+app.use("/api/auth", authRouter);
 
 //middleware for comprehensive error process
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
-  const message = err.message || 'Internal Server Error!!';
+  const message = err.message || "Internal Server Error!!";
   return res.status(statusCode).json({
     success: false,
     statusCode,
     message,
   });
-})
+});
