0e0c7a1a50f149437f1b618524c07527aff9375b
GLab <54309362+GLab-cloud@users.noreply.github.com>
2025-07-07 14:59:40 +0000

    redux addon

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,46 +1,57 @@
-import React from 'react'
-import { Link, useNavigate } from 'react-router-dom'
-import { useState } from 'react'
-
+import React from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  signInStart,
+  signInFailure,
+  signInSuccess,
+} from "../redux/user/userSlice";
 export default function SignIn() {
-  const [formData, setFormData] = useState({})
-  const [loading, setLoading] = useState(false)
-  const [error, setError] = useState(null)
-  const navigate = useNavigate()
+  const [formData, setFormData] = useState({});
+  // const [loading, setLoading] = useState(false);
+  // const [error, setError] = useState(null);
+  const { loading, error } = useSelector((state) => state.user);
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
   const handleChange = (e) => {
     setFormData({
       ...formData,
       [e.target.id]: e.target.value,
-    })
-  }
+    });
+  };
   // console.log(formData)
   const handleSubmit = async (e) => {
-    e.preventDefault()
+    e.preventDefault();
     try {
-      setLoading(true)
-      const res = await fetch('http://localhost:5000/api/auth/signin', {
-        method: 'POST',
+      //setLoading(true);
+      dispatch(signInStart());
+      const res = await fetch("http://localhost:5000/api/auth/signin", {
+        method: "POST",
         headers: {
-          'Content-Type': 'application/json',
+          "Content-Type": "application/json",
         },
         body: JSON.stringify(formData),
-      })
-      const data = await res.json()
-      console.log(data)
+      });
+      const data = await res.json();
+      console.log(data);
       if (data.success === false) {
-        setLoading(false)
-        setError(data.message)
-        return
+        // setLoading(false);
+        // setError(data.message);
+        dispatch(signInFailure(data.message));
+        return;
       }
-      setLoading(false)
-      setError(null)
-      navigate('/')
+      // setLoading(false);
+      // setError(null);
+      dispatch(signInSuccess(data));
+      navigate("/");
     } catch (error) {
-      setLoading(false)
-      setError(error.message)
+      // setLoading(false);
+      // setError(error.message);
+      dispatch(signInFailure(error.message));
     }
-  }
-  console.log(formData)
+  };
+  console.log(formData);
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl text-center font-semibold my-7">Sign in</h1>
@@ -63,16 +74,16 @@ export default function SignIn() {
           disabled={loading}
           className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-85 disabled:opacity-75"
         >
-          {loading ? 'Loading...' : 'Sign in'}
+          {loading ? "Loading..." : "Sign in"}
         </button>
       </form>
       <div className="flex gap-2 mt-2">
         <p> Dont have an account?</p>
-        <Link to={'/sign-up'}>
+        <Link to={"/sign-up"}>
           <span className="text-blue-700">Sign up</span>
         </Link>
       </div>
       {error && <p className="text-red-500 mt-5">{error}</p>}
     </div>
-  )
+  );
 }
