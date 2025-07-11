ce1574c13495557615976b3479bf936e04d271df
GLab <54309362+GLab-cloud@users.noreply.github.com>
2025-07-11 07:30:34 +0000

    profile UI

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,4 +1,46 @@
 import React from "react";
+import { useSelector } from "react-redux";
 export default function Profile() {
-  return <div>Profile</div>;
+  const { currentUser } = useSelector((state) => state.user);
+  return (
+    <div className="p-3 max-w-lg mx-auto">
+      <h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
+      <form className="flex flex-col gap-4">
+        <img
+          src={currentUser.avatar}
+          alt="Profile"
+          className="rounded-full w-25 h-25 object-cover cursor-pointer self-center mt-2"
+        />
+        <input
+          type="text"
+          placeholder="username"
+          className="p-3 border rounded-lg"
+          id="username"
+        />
+        <input
+          type="email"
+          placeholder="email"
+          className="p-3 border rounded-lg"
+          id="email"
+        />
+        <input
+          type="text"
+          placeholder="password"
+          className="p-3 border rounded-lg"
+          id="password"
+        />
+        <button
+          type="submit"
+          className="bg-slate-700 text-white rounded-lg p-3 uppercase hover:opacity-95 disabled:opacity-80"
+        >
+          {" "}
+          Update{" "}
+        </button>
+      </form>
+      <div className=" flex justify-between mt-5">
+        <span className="cursor-pointer text-red-700">Delete Account</span>
+        <span className="cursor-pointer text-red-700">Sign out</span>
+      </div>
+    </div>
+  );
 }
