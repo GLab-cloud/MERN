25c122fc955dfd15667f425730e6f52f4e5d3011
GLab <54309362+GLab-cloud@users.noreply.github.com>
2025-07-11 08:56:06 +0000

    storage

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,12 +1,42 @@
 import React from "react";
 import { useSelector } from "react-redux";
+import { useRef,useState, useEffect} from "react";
+import {getStorage} from "firebase/storage";
+import { app } from "../../firebase";
 export default function Profile() {
   const { currentUser } = useSelector((state) => state.user);
+  const fileRef = useRef(null);
+  const [file,setFile]=useState(undefined)
+  console.log(file)
+  useEffect(() => {
+    if(file){
+      handleFileUpload();
+    }
+  }, [file]);
+  const handleFileUpload=()=>{
+      const storage =getStorage(app)
+  }
+//   cloud filestore
+//   rules_version = '2';
+
+// service cloud.firestore {
+//   match /databases/{database}/documents {
+//     match /{document=**} {
+//       allow read;
+//       allow write: if 
+//       request.resource.size<2*1024*1024&&
+//       request.resource.contentType.matches('image/.*');
+//     }
+//   }
+// }
+
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
       <form className="flex flex-col gap-4">
+        <input onChange={(e)=>{setFile(e.target.files[0])}} type="file" ref={fileRef} hidden accept="image/*" />
         <img
+          onClick={() => fileRef.current.click()}
           src={currentUser.avatar}
           alt="Profile"
           className="rounded-full w-25 h-25 object-cover cursor-pointer self-center mt-2"
