30597ba26ff3d73b810a33b7f6933bfa9a496dbe
GLab <54309362+GLab-cloud@users.noreply.github.com>
2025-07-11 04:51:30 +0000

    await res.json() promise - status: pending

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -26,7 +26,7 @@ export default function Header() {
           <Link to="/about">
             <li className="sm:inline text-slate-700 hover:underline">About</li>
           </Link>
-          <Link to="/sign-in">
+          <Link to="/profile">
             {currentUser ? (
               <img
                 className="rounded-full h-7 w-7 object-cover"
