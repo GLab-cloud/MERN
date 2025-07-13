0e9fa3ecb449a9ccd208aeae5e55bea38d377877
GLab <54309362+GLab-cloud@users.noreply.github.com>
2025-07-13 10:21:07 +0000

    upload image profile to Firebase Storage

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,46 +1,98 @@
 import React from "react";
 import { useSelector } from "react-redux";
-import { useRef,useState, useEffect} from "react";
-import {getStorage} from "firebase/storage";
-import { app } from "../../firebase";
+import { useRef, useState, useEffect } from "react";
+import {
+  getStorage,
+  uploadBytesResumable,
+  ref,
+  getDownloadURL,
+} from "firebase/storage";
+import { app } from "../../firebase.js";
 export default function Profile() {
   const { currentUser } = useSelector((state) => state.user);
   const fileRef = useRef(null);
-  const [file,setFile]=useState(undefined)
-  console.log(file)
+  const [file, setFile] = useState(undefined);
+  const [filePerc, setFilePerc] = useState(0);
+  const [fileUploadError, setfileUploadError] = useState(false);
+  const [formData, setFormData] = useState({});
+  console.log(filePerc);
+  console.log(file);
+  console.log(formData);
   useEffect(() => {
-    if(file){
+    if (file) {
       handleFileUpload();
     }
   }, [file]);
-  const handleFileUpload=()=>{
-      const storage =getStorage(app)
-  }
-//   cloud filestore
-//   rules_version = '2';
 
-// service cloud.firestore {
-//   match /databases/{database}/documents {
-//     match /{document=**} {
-//       allow read;
-//       allow write: if 
-//       request.resource.size<2*1024*1024&&
-//       request.resource.contentType.matches('image/.*');
-//     }
-//   }
-// }
+  // upload profile image to firebase/storage or service cloud.firestore
+  const handleFileUpload = () => {
+    const storage = getStorage(app);
+    const fileName = new Date().getTime() + file.name;
+    const storageRef = ref(storage, fileName);
+    const uploadTask = uploadBytesResumable(storageRef, file);
+    uploadTask.on(
+      "state_changed",
+      (snapshot) => {
+        const progress =
+          (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        console.log("Upload is " + progress + "% done");
+        setFilePerc(Math.round(progress));
+      },
+      (error) => {
+        setfileUploadError(true);
+      },
+      () => {
+        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) =>
+          setFormData({ ...formData, avatar: downloadURL })
+        );
+      }
+    );
+  };
+  console.log(fileUploadError);
+  //   cloud filestore
+  //   rules_version = '2';
+
+  // service cloud.firestore {
+  //   match /databases/{database}/documents {
+  //     match /{document=**} {
+  //       allow read;
+  //       allow write: if
+  //       request.resource.size<2*1024*1024&&
+  //       request.resource.contentType.matches('image/.*');
+  //     }
+  //   }
+  // }
 
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
       <form className="flex flex-col gap-4">
-        <input onChange={(e)=>{setFile(e.target.files[0])}} type="file" ref={fileRef} hidden accept="image/*" />
+        <input
+          onChange={(e) => {
+            setFile(e.target.files[0]);
+          }}
+          type="file"
+          ref={fileRef}
+          hidden
+          accept="image/*"
+        />
         <img
           onClick={() => fileRef.current.click()}
-          src={currentUser.avatar}
+          src={formData.avatar || currentUser.avatar}
           alt="Profile"
           className="rounded-full w-25 h-25 object-cover cursor-pointer self-center mt-2"
         />
+        <p className="text-sm self-center">
+          {fileUploadError ? (
+            <span className="text-red-500"> Error Image Upload</span>
+          ) : filePerc > 0 && filePerc < 100 ? (
+            <span className="text-slate-700"> {`Uploading ${filePerc}%`}</span>
+          ) : filePerc === 100 ? (
+            <span className="text-green-700"> Image successfully loaded!</span>
+          ) : (
+            ""
+          )}
+        </p>
         <input
           type="text"
           placeholder="username"
