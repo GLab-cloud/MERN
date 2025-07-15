b4da12d993be70f95c03067205716a617dd98c44
GLab <54309362+GLab-cloud@users.noreply.github.com>
2025-07-15 08:44:55 +0000

    sign out API

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -15,6 +15,9 @@ import {
   deleteUserFailure,
   deleteUserSuccess,
   deleteUserStart,
+  signOutUserFailure,
+  signOutUserSuccess,
+  signOutUserStart,
 } from "../redux/user/userSlice.js";
 import { useDispatch } from "react-redux";
 
@@ -151,7 +154,21 @@ export default function Profile() {
       dispatch(deleteUserFailure(error.message));
     }
   };
-
+  const handleSignOut = async () => {
+    try {
+      dispatch(signOutUserStart());
+      const res = await fetch("https://localhost:5000/api/auth/signout", {
+        credentials: "include",
+      });
+      const data = await res.json(); //
+      if (data.success === false) {
+        dispatch(signOutUserFailure(data.message));
+      }
+      dispatch(signOutUserSuccess(data));
+    } catch (error) {
+      dispatch(signOutUserFailure(error.message));
+    }
+  };
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
@@ -224,7 +241,9 @@ export default function Profile() {
         >
           Delete Account
         </span>
-        <span className="cursor-pointer text-red-700">Sign out</span>
+        <span onClick={handleSignOut} className="cursor-pointer text-red-700">
+          Sign out
+        </span>
       </div>
       <p className="text-red-700 mt-5">{error ? error : ""}</p>
       <p className="text-green-700 mt-5">
