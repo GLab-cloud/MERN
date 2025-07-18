28944cfa998a5c807fae67c55ca65e91cd57b73e
GLab <54309362+GLab-cloud@users.noreply.github.com>
2025-07-18 12:23:17 +0000

    UI create listing

--- client/src/pages/createListing.jsx
+++ client/src/pages/createListing.jsx
@@ -8,7 +8,7 @@ export default function CreateListing() {
         Create a Listing
       </h1>
       <form className="flex flex-col sm:flex-row">
-        <div className="flex flex-col gap-4 flex-1">
+        <div className="flex flex-col flex-1 gap-4 ">
           <input
             placeholder="Name"
             type="text"
@@ -33,64 +33,102 @@ export default function CreateListing() {
             id="address"
             required
           />
-          <div className="">
-            <div className="flex gap-2">
-              <input className="w-5" type="checkbox" id="sale" />
-              <span>Sell</span>
-            </div>
-            <div className="flex gap-2">
-              <input className="w-5" type="checkbox" id="rent" />
-              <span>Rent</span>
-            </div>
-            <div className="flex gap-2">
-              <input className="w-5" type="checkbox" id="parking" />
-              <span>Parking spot</span>
-            </div>
-            <div className="flex gap-2">
-              <input className="w-5" type="checkbox" id="furnished" />
-              <span>Furnished</span>
-            </div>
-            <div className="flex gap-2">
-              <input className="w-5" type="checkbox" id="offer" />
-              <span>Offer</span>
-            </div>
-            <div className="">
-              <div className="flex items-center gap-2">
-                <input
-                  type="number"
-                  id="bedrooms"
-                  max="10"
-                  min="1"
-                  required
-                  className="p-3 border rounded-lg border-gray-300"
-                />
-                <p> Beds</p>
-              </div>
-              <div className="flex items-center gap-2">
-                <input
-                  type="number"
-                  id="bathrooms"
-                  max="10"
-                  min="1"
-                  required
-                  className="p-3 border rounded-lg border-gray-300"
-                />
-                <p> Baths</p>
-              </div>
-              <div className="flex items-center gap-2">
-                <input
-                  type="number"
-                  id="regularPrice"
-                  required
-                  className="p-3 border rounded-lg border-gray-300"
-                />
-                <div className="">
-                  <p> Regular Price</p>
-                  <span>($/ month)</span>
-                </div>
-              </div>
+        </div>
+        <div className="flex gap-6 flex-wrap mt-3">
+          <div className="flex gap-2">
+            <input className="w-5" type="checkbox" id="sale" />
+            <span>Sell</span>
+          </div>
+          <div className="flex gap-2">
+            <input className="w-5" type="checkbox" id="rent" />
+            <span>Rent</span>
+          </div>
+          <div className="flex gap-2">
+            <input className="w-5" type="checkbox" id="parking" />
+            <span>Parking spot</span>
+          </div>
+          <div className="flex gap-2">
+            <input className="w-5" type="checkbox" id="furnished" />
+            <span>Furnished</span>
+          </div>
+          <div className="flex gap-2">
+            <input className="w-5" type="checkbox" id="offer" />
+            <span>Offer</span>
+          </div>
+        </div>
+        <div className="flex flex-wrap gap-6 mt-3">
+          <div className="flex items-center gap-2">
+            <input
+              type="number"
+              id="bedrooms"
+              max="10"
+              min="1"
+              required
+              className="p-3 border rounded-lg border-gray-300"
+            />
+            <p> Beds</p>
+          </div>
+          <div className="flex items-center gap-2">
+            <input
+              type="number"
+              id="bathrooms"
+              max="10"
+              min="1"
+              required
+              className="p-3 border rounded-lg border-gray-300"
+            />
+            <p> Baths</p>
+          </div>
+          <div className="flex items-center gap-2">
+            <input
+              type="number"
+              id="regularPrice"
+              required
+              className="p-3 border rounded-lg border-gray-300"
+            />
+            <div className="flex flex-col items-center">
+              <p> Regular Price</p>
+              <span className="text-xs">($/ month)</span>
             </div>
           </div>
+          <div className="flex items-center gap-2">
+            <input
+              type="number"
+              id="discountPrice"
+              required
+              className="p-3 border rounded-lg border-gray-300"
+            />
+            <div className="flex flex-col items-center">
+              <p> Discount Price</p>
+              <span className="text-xs">($/ month)</span>
+            </div>
+          </div>
+        </div>
+        <div className=" flex flex-col flex-1 gap-4">
+          <p className="font-semibold">
+            {" "}
+            Images:{" "}
+            <span className="font-normal text-gray-600 ml-2">
+              {" "}
+              The first image will be the cover (max - 6)
+            </span>
+          </p>
+          <div className="flex gap-4">
+            <input
+              type="file"
+              accept="image/*"
+              multiple
+              id="images"
+              className="p-3 border border-gray-300 rounded-w-full "
+            />
+            <button className=" p-3 text-green-700 border-green-700 rounded uppercase hover: shadow-lg disabled: opacity-80">
+              Upload
+            </button>
+          </div>
+          <button className="bg-slate-700 text-white rounded-lg uppercase hover:opacity-80 disabled:opacity-80">
+            {" "}
+            Create Listing
+          </button>
         </div>
       </form>
     </main>
