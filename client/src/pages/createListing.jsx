7f044b6ca9a852a335ceb316115b55216d6e6089
GLab <54309362+GLab-cloud@users.noreply.github.com>
2025-07-19 03:44:26 +0000

    upload task

--- client/src/pages/createListing.jsx
+++ client/src/pages/createListing.jsx
@@ -1,6 +1,51 @@
-import React from "react";
-
+import React, { useState } from "react";
+import {
+  getStorage,
+  uploadBytesResumable,
+  ref,
+  getDownloadURL,
+} from "firebase/storage";
+import { app } from "../../firebase.js";
 export default function CreateListing() {
+  const [files, setFiles] = useState([]);
+  const [formData, setFormData] = useState({ imageUrls: [] });
+  console.log(files);
+  const handleImageSubmit = (e) => {
+    if (files.length > 0 && files.length < 7) {
+      const promises = [];
+      for (let i = 0; i < files.length; i++) {
+        promises.push(storeImage(files[i]));
+      }
+      Promise.all(promises).then((urls) => {
+        setFormData({
+          ...formData,
+          imageUrls: formData.imageUrls.concat(urls),
+        });
+      });
+    }
+  };
+  const storeImage = async (file) => {
+    return new Promise((resolve, reject) => {
+      const storage = getStorage(app);
+      const fileName = new Date().getTime() + file.name;
+      const storageRef = ref(storage, fileName);
+      const uploadTask = uploadBytesResumable(storageRef, file);
+      uploadTask.on(
+        "state_changed",
+        (snapshot)=>{
+          const progress=(snapshot.bytesTransferred/snapshot.totalBytes)*100;
+          console.log(`Upload is ${progress}% done`)
+        }
+        (error) => {
+          reject(error);
+        },
+        () => {getDownloadURL(uploadTask.snapshot.ref).then((downloadUrl)=>{resolve(downloadUrl)
+
+        })
+      }
+      );
+    });
+  };
   return (
     <main className="p-3 max-w-4xl mx-auto">
       <h1 className="text-3xl font-semibold text-center my-7">
@@ -115,6 +160,9 @@ export default function CreateListing() {
           </p>
           <div className="flex gap-4">
             <input
+              onChange={(e) => {
+                setFiles(e.target.files);
+              }}
               type="file"
               accept="image/*"
               multiple
