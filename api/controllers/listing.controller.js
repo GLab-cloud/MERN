2a1c74eb162d062e4c25ddf745a824cc54f45e21
GLab <54309362+GLab-cloud@users.noreply.github.com>
2025-08-03 08:44:53 +0000

    update listing API

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -12,18 +12,41 @@ export const createListing = async (req, res, next) => {
 export const deleteListing = async (req, res, next) => {
   const listing = Listing.findById(req.params.id);
   if (!listing) {
-    return next(errorHandler(401, "Listing not found"));
+    return next(errorHandler(404, "Listing not found"));
   }
 
   if (req.user._id !== listing.userRef) {
     console.log(req.user.id);
     console.log(listing.userRef);
 
-    return next(errorHandler(401, "You can delete your own listing"));
+    return next(errorHandler(401, "You can only delete your own listing"));
   }
   try {
     await Listing.findByIdAndDelete(req.params.id);
     res.status(200).json("Listing has been deleted");
+  } catch (error) {
+    next(error);
+  }
+};
+export const updateListing = async (req, res, next) => {
+  const listing = Listing.findById(req.params.id);
+  if (!listing) {
+    return next(errorHandler(404, "Listing not found"));
+  }
+
+  if (req.user._id !== listing.userRef) {
+    console.log(req.user.id);
+    console.log(listing.userRef);
+
+    return next(errorHandler(401, "You can only update your own listing"));
+  }
+  try {
+    const updateListing = await Listing.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true }
+    );
+    res.status(200).json(updateListing);
   } catch (error) {
     next(error);
   }
