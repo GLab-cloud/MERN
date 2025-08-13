ebd6b28c42adb35ada121b970f8cbadca8ccca7f
GLab <54309362+GLab-cloud@users.noreply.github.com>
2025-08-13 04:56:38 +0000

    Listing styling

--- client/src/pages/Listing.jsx
+++ client/src/pages/Listing.jsx
@@ -67,17 +67,24 @@ export default function Listing() {
       {listing && !loading & !error && (
         <div>
           <Swiper navigation>
-            {listing.imageUrls.map((url) => (
-              <SwiperSlide key={url}>
-                <div
-                  className="h-[550px]"
-                  style={{
-                    background: `url(${url}) center no-repeat`,
-                    backgroundSize: "cover",
-                  }}
-                ></div>
-              </SwiperSlide>
-            ))}
+            {listing.imageUrls[0] ? (
+              listing.imageUrls.map((url) => (
+                <SwiperSlide key={url}>
+                  <div
+                    className="h-[550px]"
+                    style={{
+                      background: `url(${url}) center no-repeat`,
+                      backgroundSize: "cover",
+                    }}
+                  ></div>
+                </SwiperSlide>
+              ))
+            ) : (
+              <img
+                className="w-full h-[430px] flex justify-center mt-2 rounded-lg p-2 object-cover"
+                src="https://rentapartment.vn/wp-content/uploads/2021/03/Real-estate-la-gi.jpg"
+              />
+            )}
           </Swiper>
           <div className="fixed top-[13%] right-[3%] z-10 border rounded-full w-12 h-12 flex justify-center items-center bg-slate-100 cursor-pointer">
             <FaShare
